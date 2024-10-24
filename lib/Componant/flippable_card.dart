4a1fde3e1c6e063d94e603e0da8fbffbbe824f16
moritejas <163624119+moritejas@users.noreply.github.com>
2024-10-24 17:16:27 +0530

    Flippable Card Component

--- lib/Componant/flippable_card.dart
+++ lib/Componant/flippable_card.dart
@@ -1,3 +1,4 @@
+import 'package:flip_card/flip_card.dart';
 import 'package:flutter/material.dart';
 
 class FlippableCardComponent extends StatelessWidget {
@@ -8,8 +9,62 @@ class FlippableCardComponent extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
       ),
-      body: const Text("This is Column Component"),
-
+      body: SafeArea(
+        top: true,
+        child: FlipCard(
+          fill: Fill.fillBack,
+          direction: FlipDirection.HORIZONTAL,
+          speed: 400,
+          front: Container(
+            width: 100,
+            height: 100,
+            decoration: const BoxDecoration(
+              color: Colors.deepPurple,
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(12),
+                bottomRight: Radius.circular(12),
+                topLeft: Radius.circular(12),
+                topRight: Radius.circular(12),
+              ),
+            ),
+            child: const Align(
+              alignment: AlignmentDirectional(0, 0),
+              child: Text(
+                'Front',
+                style: TextStyle(
+                  fontFamily: 'Inter',
+                  color: Colors.white,
+                  letterSpacing: 0.0,
+                ),
+              ),
+            ),
+          ),
+          back: Container(
+            width: 100,
+            height: 100,
+            decoration: const BoxDecoration(
+              color: Colors.deepPurpleAccent,
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(12),
+                bottomRight: Radius.circular(12),
+                topLeft: Radius.circular(12),
+                topRight: Radius.circular(12),
+              ),
+            ),
+            child: const Align(
+              alignment: AlignmentDirectional(0, 0),
+              child: Text(
+                'Back',
+                style: TextStyle(
+                  fontFamily: 'Inter',
+                  color: Colors.white,
+                  letterSpacing: 0.0,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
