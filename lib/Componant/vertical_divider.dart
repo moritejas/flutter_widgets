2b297cf34e7f86464067ddab4538d19586e9f36e
moritejas <163624119+moritejas@users.noreply.github.com>
2024-10-24 11:58:41 +0530

    Vertical Divider

--- lib/Componant/vertical_divider.dart
+++ lib/Componant/vertical_divider.dart
@@ -8,8 +8,29 @@ class VerticalDividerComponent extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
       ),
-      body: const Center(
-        child: Text("Welcome to Flutter Application "),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            height: 100,
+            width: 100,
+            color: Colors.red,
+            child: const Center(child: Text('Box 1')),
+          ),
+          const VerticalDivider(
+            color: Colors.black,
+            thickness: 2, // Width of the divider
+            width: 20,    // Space around the divider
+            indent: 10,   // Space at the top
+            endIndent: 10, // Space at the bottom
+          ),
+          Container(
+            height: 100,
+            width: 100,
+            color: Colors.green,
+            child: const Center(child: Text('Box 2')),
+          ),
+        ],
       ),
     );
   }
