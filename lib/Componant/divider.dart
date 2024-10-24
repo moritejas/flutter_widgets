71857ab590e8fee224ae5ccdd6a51489568a4784
moritejas <163624119+moritejas@users.noreply.github.com>
2024-10-24 11:55:08 +0530

    Spacer

--- lib/Componant/divider.dart
+++ lib/Componant/divider.dart
@@ -6,10 +6,35 @@ class DividerComponent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-      ),
-      body: const Center(
-        child: Text("Welcome to Flutter Application "),
+      appBar: AppBar(),
+      body: const Column(
+        children: [
+          ListTile(
+            leading: Icon(Icons.person),
+            title: Text('Person 1'),
+          ),
+          Divider(
+            // Horizontal Divider
+            color: Colors.black,
+            thickness: 2, // Thickness of the line
+            indent: 20, // Space before the divider
+            endIndent: 20, // Space after the divider
+          ),
+          ListTile(
+            leading: Icon(Icons.person),
+            title: Text('Person 2'),
+          ),
+          Divider(
+            color: Colors.grey,
+            thickness: 1,
+            indent: 20,
+            endIndent: 20,
+          ),
+          ListTile(
+            leading: Icon(Icons.person),
+            title: Text('Person 3'),
+          ),
+        ],
       ),
     );
   }
