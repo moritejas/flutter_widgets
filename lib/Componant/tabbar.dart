515d4a56fa6a3f5cddf81a78dce50ffff584774e
moritejas <163624119+moritejas@users.noreply.github.com>
2024-10-24 12:03:36 +0530

    TabBar

--- lib/Componant/tabbar.dart
+++ lib/Componant/tabbar.dart
@@ -5,11 +5,34 @@ class TabBarComponent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-      ),
-      body: const Center(
-        child: Text("Welcome to Flutter Application "),
+    return DefaultTabController(
+      length: 2, // Number of tabs
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Simple TabBar Example'),
+          bottom: const TabBar(
+            tabs: [
+              Tab(text: 'Tab 1'), // First tab
+              Tab(text: 'Tab 2'), // Second tab
+            ],
+          ),
+        ),
+        body: const TabBarView(
+          children: [
+            Center(
+              child: Text(
+                'Content for Tab 1',
+                style: TextStyle(fontSize: 24),
+              ),
+            ),
+            Center(
+              child: Text(
+                'Content for Tab 2',
+                style: TextStyle(fontSize: 24),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
