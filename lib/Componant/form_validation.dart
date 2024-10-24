c53d90929c6bc16072c8445d2a2279f4d9bf09ab
moritejas <163624119+moritejas@users.noreply.github.com>
2024-10-24 14:26:29 +0530

    FormValidation

--- lib/Componant/form_validation.dart
+++ lib/Componant/form_validation.dart
@@ -1,15 +1,78 @@
 import 'package:flutter/material.dart';
 
-class FormValidationComponent extends StatelessWidget {
+class FormValidationComponent extends StatefulWidget {
   const FormValidationComponent({super.key});
+
+  @override
+  State<FormValidationComponent> createState() =>
+      _FormValidationComponentState();
+}
+
+class _FormValidationComponentState extends State<FormValidationComponent> {
+  final _formKey = GlobalKey<FormState>();
+
+  String? _name;
+
+  String? _email;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-      ),
-      body: const Center(
-        child: Text("Welcome to Flutter Application "),
+      appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Name'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your name';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  _name = value;
+                },
+              ),
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Email'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your email';
+                  }
+                  // Regular expression for validating email
+                  final RegExp emailRegExp = RegExp(
+                    r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$',
+                  );
+                  if (!emailRegExp.hasMatch(value)) {
+                    return 'Please enter a valid email';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  _email = value;
+                },
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    // If the form is valid, save the data
+                    _formKey.currentState!.save();
+                    // You can now use _name and _email
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text('Name: $_name, Email: $_email')),
+                    );
+                  }
+                },
+                child: const Text('Submit'),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
