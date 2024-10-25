97e768ebe28398732b7b454128ccc09dd6b71ea3
moritejas <163624119+moritejas@users.noreply.github.com>
2024-10-25 10:41:50 +0530

    Circle Image

--- lib/Componant/circle_image.dart
+++ lib/Componant/circle_image.dart
@@ -8,8 +8,23 @@ class CilrcleImageComponent extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
       ),
-      body: const Text("This is Column Component"),
-
+      body: SafeArea(
+        top: true,
+        child: Center(
+          child: Container(
+            width: 200,
+            height: 200,
+            clipBehavior: Clip.antiAlias,
+            decoration: const BoxDecoration(
+              shape: BoxShape.circle,
+            ),
+            child: Image.network(
+              'https://picsum.photos/seed/145/600',
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
