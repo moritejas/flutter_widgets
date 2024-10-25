e27f9683dda358faae6744dcef840916fd10a4a4
moritejas <163624119+moritejas@users.noreply.github.com>
2024-10-25 13:09:01 +0530

    Video Player

--- lib/Componant/videoplayer.dart
+++ lib/Componant/videoplayer.dart
@@ -1,16 +1,72 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
-class VideoPlayerComponent extends StatelessWidget {
+class VideoPlayerComponent extends StatefulWidget {
   const VideoPlayerComponent({super.key});
+
+  @override
+  _VideoPlayerComponentState createState() => _VideoPlayerComponentState();
+}
+
+class _VideoPlayerComponentState extends State<VideoPlayerComponent> {
+  late VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    // Initialize the video player with a sample video
+    _controller = VideoPlayerController.network(
+        'https://assets.mixkit.co/videos/preview/mixkit-forest-stream-in-the-sunlight-529-large.mp4')
+      ..initialize().then((_) {
+        // Ensure the first frame is shown after the video is initialized
+        setState(() {});
+      });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    // Dispose of the controller when the widget is removed
+    _controller.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-        ),
-        body: const Card(
-          child: Text("This is Card Component"),
+      appBar: AppBar(title: Text("Video Player")),
+      body: Center(
+        child: _controller.value.isInitialized
+            ? Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            AspectRatio(
+              aspectRatio: _controller.value.aspectRatio,
+              child: VideoPlayer(_controller),
+            ),
+            VideoProgressIndicator(_controller, allowScrubbing: true),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                  icon: Icon(
+                    _controller.value.isPlaying
+                        ? Icons.pause
+                        : Icons.play_arrow,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      _controller.value.isPlaying
+                          ? _controller.pause()
+                          : _controller.play();
+                    });
+                  },
+                ),
+              ],
+            ),
+          ],
         )
+            : CircularProgressIndicator(),
+      ),
     );
   }
 }
