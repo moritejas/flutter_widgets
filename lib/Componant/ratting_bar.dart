2d82fab945079b5ba0e51ec42d20e77a97089175
moritejas <163624119+moritejas@users.noreply.github.com>
2024-10-28 13:03:00 +0530

    Ratting Bar

--- lib/Componant/ratting_bar.dart
+++ lib/Componant/ratting_bar.dart
@@ -1,13 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
-class RattingBarComponent extends StatelessWidget {
+class RattingBarComponent extends StatefulWidget {
   const RattingBarComponent({super.key});
+
+  @override
+  State<RattingBarComponent> createState() => _RattingBarComponentState();
+}
+
+class _RattingBarComponentState extends State<RattingBarComponent> {
+  double _rating = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(" This is App Bar"),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Rating: ${_rating.toStringAsFixed(1)}',
+              style: const TextStyle(fontSize: 24.0),
+            ),
+            const SizedBox(height: 20),
+
+            /// Add This Dependencies
+            /// flutter_rating_bar: ^4.0.1
+
+            RatingBar.builder(
+              // initialRating: _rating,
+              // minRating: 1,
+              // direction: Axis.horizontal,
+              // allowHalfRating: true,
+              // itemCount: 5,
+              // itemPadding: const EdgeInsets.symmetric(horizontal: 4.0),
+              itemBuilder: (context, _) => const Icon(
+                Icons.star,
+                color: Colors.amber,
+              ),
+              onRatingUpdate: (rating) {
+                setState(() {
+                  _rating = rating;
+                });
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
