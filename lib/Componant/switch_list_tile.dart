823b663cbbca94536961c7c9a015d63e286adcd5
moritejas <163624119+moritejas@users.noreply.github.com>
2024-10-28 12:30:26 +0530

    SwitchListTile

--- lib/Componant/switch_list_tile.dart
+++ lib/Componant/switch_list_tile.dart
@@ -1,13 +1,29 @@
 import 'package:flutter/material.dart';
 
-class SwitchListTileComponent extends StatelessWidget {
+class SwitchListTileComponent extends StatefulWidget {
   const SwitchListTileComponent({super.key});
+
+  @override
+  State<SwitchListTileComponent> createState() => _SwitchListTileComponentState();
+}
+
+class _SwitchListTileComponentState extends State<SwitchListTileComponent> {
+  // Boolean variable to track the switch state
+  bool isSwitched = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(" This is App Bar"),
+      body: SwitchListTile(
+        title: const Text('Enable Notifications'),
+        subtitle: const Text('Receive updates and alerts'),
+        value: isSwitched,
+        onChanged: (bool value) {
+          setState(() {
+            isSwitched = value;
+          });
+        },
+        secondary: const Icon(Icons.notifications),
       ),
     );
   }
