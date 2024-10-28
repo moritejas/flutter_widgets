9098fb3b9fa5d4104a937338517079ef945a1320
moritejas <163624119+moritejas@users.noreply.github.com>
2024-10-28 13:13:33 +0530

    Choice Chip

--- lib/Componant/choice_chip.dart
+++ lib/Componant/choice_chip.dart
@@ -1,13 +1,36 @@
 import 'package:flutter/material.dart';
 
-class ChoiceChipComponent extends StatelessWidget {
+class ChoiceChipComponent extends StatefulWidget {
   const ChoiceChipComponent({super.key});
 
   @override
+  State<ChoiceChipComponent> createState() => _ChoiceChipComponentState();
+}
+
+class _ChoiceChipComponentState extends State<ChoiceChipComponent> {
+  // List of fruits to choose from
+  final List<String> fruits = ['Apple', 'Banana', 'Cherry', 'Date'];
+
+  String? selectedFruit;
+  // Variable to track the selected fruit
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(" This is App Bar"),
+      body: Center(
+        child: Wrap(
+          spacing: 8.0, // Space between chips
+          children: fruits.map((fruit) {
+            return ChoiceChip(
+              label: Text(fruit),
+              selected: selectedFruit == fruit,
+              onSelected: (bool selected) {
+                setState(() {
+                  selectedFruit = selected ? fruit : null;
+                });
+              },
+            );
+          }).toList(),
+        ),
       ),
     );
   }
