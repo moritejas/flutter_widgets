41b6ddb81ebe747d28641693fcada2044d005159
moritejas <163624119+moritejas@users.noreply.github.com>
2024-10-28 12:14:28 +0530

    CheckBox

--- lib/Componant/checkbox.dart
+++ lib/Componant/checkbox.dart
@@ -1,13 +1,26 @@
 import 'package:flutter/material.dart';
 
-class CheckBoxComponent extends StatelessWidget {
+class CheckBoxComponent extends StatefulWidget {
   const CheckBoxComponent({super.key});
 
   @override
+  State<CheckBoxComponent> createState() => _CheckBoxComponentState();
+}
+
+class _CheckBoxComponentState extends State<CheckBoxComponent> {
+  bool isChecked = false;
+
+  @override
+
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(" This is App Bar"),
+      body: Checkbox(
+        value: isChecked,
+        onChanged: (bool? value) {
+          setState(() {
+            isChecked = value ?? false;
+          });
+        },
       ),
     );
   }
