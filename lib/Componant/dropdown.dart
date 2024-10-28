38f4db250d7aa88d52089a5a46ceac8bbc8f93e9
moritejas <163624119+moritejas@users.noreply.github.com>
2024-10-28 12:09:11 +0530

    Drop Down

--- lib/Componant/dropdown.dart
+++ lib/Componant/dropdown.dart
@@ -1,13 +1,33 @@
 import 'package:flutter/material.dart';
 
-class DropDownComponent extends StatelessWidget {
+class DropDownComponent extends StatefulWidget {
   const DropDownComponent({super.key});
+
+  @override
+  State<DropDownComponent> createState() => _DropDownComponentState();
+}
+
+class _DropDownComponentState extends State<DropDownComponent> {
+  String? dropdownValue;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(" This is App Bar"),
+      body: DropdownButton<String>(
+        items: <String>['Option 1', 'Option 2', 'Option 3','Option 4', 'Option 5']
+            .map<DropdownMenuItem<String>>((String value) {
+          return DropdownMenuItem<String>(
+            value: value,
+            child: Text(value),
+          );
+        }).toList(),
+        onChanged: (String? newValue) {
+          setState(() {
+            dropdownValue = newValue;
+          });
+        },
+        elevation: 2,
+        // borderColor: Colors.transparent,
       ),
     );
   }
