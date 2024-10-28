d9eaee3dc9ab2d8e1ee0ed15f2c6a52c0bfe44c4
moritejas <163624119+moritejas@users.noreply.github.com>
2024-10-28 12:53:54 +0530

    Slider

--- lib/Componant/slider.dart
+++ lib/Componant/slider.dart
@@ -1,13 +1,28 @@
 import 'package:flutter/material.dart';
 
-class SliderComponent extends StatelessWidget {
+class SliderComponent extends StatefulWidget {
   const SliderComponent({super.key});
+
+  @override
+  State<SliderComponent> createState() => _SliderComponentState();
+}
+
+class _SliderComponentState extends State<SliderComponent> {
+  // Variable to track the slider value
+  double _sliderValue = 0.0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(" This is App Bar"),
+      body: Slider(
+        value: _sliderValue,
+        min: 0.0,
+        max: 10.0,
+        onChanged: (double newValue) {
+          setState(() {
+            _sliderValue = newValue;
+          });
+        },
       ),
     );
   }
