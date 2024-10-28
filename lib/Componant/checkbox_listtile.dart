09b6b62d3f671db918cc7e6d4bcc9e2bd53106bc
moritejas <163624119+moritejas@users.noreply.github.com>
2024-10-28 12:19:03 +0530

    Checkbox ListTile

--- lib/Componant/checkbox_listtile.dart
+++ lib/Componant/checkbox_listtile.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart';
 
-class CheckBoxListTileComponent extends StatelessWidget {
+class CheckBoxListTileComponent extends StatefulWidget {
   const CheckBoxListTileComponent({super.key});
 
   @override
+  State<CheckBoxListTileComponent> createState() => _CheckBoxListTileComponentState();
+}
+
+class _CheckBoxListTileComponentState extends State<CheckBoxListTileComponent> {
+  bool isChecked = false;
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(" This is App Bar"),
-      ),
+     body: CheckboxListTile(
+       title: const Text('Accept Terms & Conditions'),
+       subtitle: const Text('Please accept the terms before proceeding.'),
+       value: isChecked,
+       onChanged: (bool? value) {
+         setState(() {
+           isChecked = value ?? false;
+         });
+       },
+       controlAffinity: ListTileControlAffinity.leading, // Checkbox on the left side
+     ),
     );
   }
 }
