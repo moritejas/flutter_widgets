b7d73f45c7fc036ace74ac2c464bcdf337cb6ae2
moritejas <163624119+moritejas@users.noreply.github.com>
2024-10-28 12:34:31 +0530

    Radio Button

--- lib/Componant/radio_button.dart
+++ lib/Componant/radio_button.dart
@@ -1,13 +1,63 @@
 import 'package:flutter/material.dart';
 
-class RadioButtonComponent extends StatelessWidget {
+class RadioButtonComponent extends StatefulWidget {
   const RadioButtonComponent({super.key});
+
+  @override
+  State<RadioButtonComponent> createState() => _RadioButtonComponentState();
+}
+
+class _RadioButtonComponentState extends State<RadioButtonComponent> {
+  // Enum to define the options for the radio buttons
+  String? selectedOption;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(" This is App Bar"),
+      body: Column(
+        children: [
+          ListTile(
+            title: const Text('Option 1'),
+            leading: Radio<String>(
+              value: 'Option 1',
+              groupValue: selectedOption,
+              onChanged: (String? value) {
+                setState(() {
+                  selectedOption = value;
+                });
+              },
+            ),
+          ),
+          ListTile(
+            title: const Text('Option 2'),
+            leading: Radio<String>(
+              value: 'Option 2',
+              groupValue: selectedOption,
+              onChanged: (String? value) {
+                setState(() {
+                  selectedOption = value;
+                });
+              },
+            ),
+          ),
+          ListTile(
+            title: const Text('Option 3'),
+            leading: Radio<String>(
+              value: 'Option 3',
+              groupValue: selectedOption,
+              onChanged: (String? value) {
+                setState(() {
+                  selectedOption = value;
+                });
+              },
+            ),
+          ),
+          const SizedBox(height: 20),
+          Text(
+            'Selected: ${selectedOption ?? "None"}',
+            style: const TextStyle(fontSize: 18),
+          ),
+        ],
       ),
     );
   }
