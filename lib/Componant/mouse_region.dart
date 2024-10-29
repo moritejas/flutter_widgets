652d8e60cbd34c6ba70e053947b18a193494d257
moritejas <163624119+moritejas@users.noreply.github.com>
2024-10-29 12:53:39 +0530

    Mouse Region

--- lib/Componant/mouse_region.dart
+++ lib/Componant/mouse_region.dart
@@ -1,15 +1,51 @@
 import 'package:flutter/material.dart';
 
-class MouseRegionComponent extends StatelessWidget {
+class MouseRegionComponent extends StatefulWidget {
   const MouseRegionComponent({super.key});
+
+  @override
+  State<MouseRegionComponent> createState() => _MouseRegionComponentState();
+}
+
+class _MouseRegionComponentState extends State<MouseRegionComponent> {
+
+  Color _color = Colors.blue;
+
+  void _onEnter(PointerEvent details) {
+    setState(() {
+      _color = Colors.green; // Change color when the mouse enters
+    });
+  }
+
+  void _onExit(PointerEvent details) {
+    setState(() {
+      _color = Colors.blue; // Reset color when the mouse exits
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
       ),
-      body: ElevatedButton(onPressed: (){},
-          child: const Text("This is Eleveted Button Component"))
+      body: Center(
+        child: MouseRegion(
+          onEnter: _onEnter,
+          onExit: _onExit,
+          child: AnimatedContainer(
+            duration: const Duration(milliseconds: 200),
+            width: 200,
+            height: 200,
+            color: _color,
+            child: const Center(
+              child: Text(
+                "Hover over me!",
+                style: TextStyle(color: Colors.white, fontSize: 18),
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
