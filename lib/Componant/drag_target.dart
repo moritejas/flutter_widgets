34da140703b2f94999a4052dab17e8406fe52179
moritejas <163624119+moritejas@users.noreply.github.com>
2024-10-29 15:03:24 +0530

    Drag Target

--- lib/Componant/drag_target.dart
+++ lib/Componant/drag_target.dart
@@ -1,15 +1,42 @@
 import 'package:flutter/material.dart';
 
-class DragTargetComponent extends StatelessWidget {
+class DragTargetComponent extends StatefulWidget {
   const DragTargetComponent({super.key});
+
+  @override
+  State<DragTargetComponent> createState() => _DragTargetComponentState();
+}
+
+class _DragTargetComponentState extends State<DragTargetComponent> {
+  Color targetColor  = Colors.grey;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        title: const Text("DragTarget"),
       ),
-      body: ElevatedButton(onPressed: (){},
-          child: const Text("This is Eleveted Button Component"))
+      body: Center(
+        child: DragTarget<Color>(
+          onAccept: (color) {
+            setState(() {
+              targetColor = color;
+            });
+          },
+          onWillAccept: (data) => data is Color,
+          builder: (context, acceptedData, rejectedData) => Container(
+            width: 200,
+            height: 200,
+            color: targetColor,
+            child: const Center(
+              child: Text(
+                "Drop Here",
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/Componant/draggable.dart
+++ lib/Componant/draggable.dart
@@ -1,34 +1,89 @@
 import 'package:flutter/material.dart';
 
-class DraggableComponent extends StatelessWidget {
+/*return Scaffold(
+      appBar: AppBar(
+        title: const Text("DragTarget"),
+      ),
+      body: Center(
+        child: DragTarget<Color>(
+          onAccept: (color) {
+            setState(() {
+              targetColor = color;
+            });
+          },
+          onWillAccept: (data) => data is Color,
+          builder: (context, acceptedData, rejectedData) => Container(
+            width: 200,
+            height: 200,
+            color: targetColor,
+            child: const Center(
+              child: Text(
+                "Drop Here",
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );*/
+
+class DraggableComponent extends StatefulWidget {
   const DraggableComponent({super.key});
+
+  @override
+  State<DraggableComponent> createState() => _DraggableComponentState();
+}
+
+class _DraggableComponentState extends State<DraggableComponent> {
+  Color caughtColor = Colors.grey;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
       ),
-      body: const SafeArea(
-        top: true,
-        child: Draggable<String>(
-          data: '',
-          feedback: Material(
-            type: MaterialType.transparency,
-            child: Text(
-              'Hello World',
-              style: TextStyle(
-                fontFamily: 'Inter',
-                letterSpacing: 0.0,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Draggable<Color>(
+              data: Colors.blue,
+              feedback: Container(
+                width: 100,
+                height: 100,
+                color: Colors.blue,
+              ),
+              childWhenDragging: Container(
+                width: 100,
+                height: 100,
+                color: Colors.blue.withOpacity(0.5),
+              ),
+              child: Container(
+                width: 100,
+                height: 100,
+                color: Colors.blue,
               ),
             ),
-          ),
-          child: Text(
-            'Hello World',
-            style: TextStyle(
-              fontFamily: 'Inter',
-              letterSpacing: 0.0,
+            SizedBox(height: 50),
+            DragTarget<Color>(
+              onAccept: (color) {
+                setState(() {
+                  caughtColor = color;
+                });
+              },
+              builder: (context, acceptedData, rejectedData) => Container(
+                width: 200,
+                height: 200,
+                color: caughtColor,
+                child: Center(
+                  child: Text(
+                    "Drop Here",
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                ),
+              ),
             ),
-          ),
+          ],
         ),
       ),
     );
