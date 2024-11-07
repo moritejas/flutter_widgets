168733c542e2d073807ad02e51731172ffb7550a
moritejas <163624119+moritejas@users.noreply.github.com>
2024-11-07 14:40:37 +0530

    IconComponent with Properties

--- lib/Component/Icon/icon.dart
+++ lib/Component/Icon/icon.dart
@@ -6,18 +6,45 @@ class IconComponent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-      ),
-      body:const Row(
+      appBar: AppBar(),
+      body: const Row(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Icon(Icons.settings),
+
+          // Divider with all properties
+          VerticalDivider(
+            color: Colors.black,              // Color of the divider line
+            thickness: 2,                     // Thickness of the divider line
+            width: 20,                        // Space occupied by the divider itself
+            indent: 10,                       // Starting indentation from top
+            endIndent: 10,                    // Ending indentation from bottom
+          ),
+
           Icon(Icons.dashboard),
+
+          VerticalDivider(
+            color: Colors.red,                // Example of a different color
+            thickness: 2,
+            width: 20,
+            indent: 10,
+            endIndent: 10,
+          ),
+
           Icon(Icons.person),
+
+          VerticalDivider(
+            color: Colors.blue,               // Example with another color
+            thickness: 2,
+            width: 20,
+            indent: 10,
+            endIndent: 10,
+          ),
+
           Icon(Icons.add),
         ],
-      )
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'Component/Flex/flex.dart';
 import 'Component/Flippable Card/flippable_card.dart';
 import 'Component/Form Validation/form_validation.dart';
 import 'Component/Grid View/grid_view.dart';
+import 'Component/Icon/icon.dart';
 
 
 void main() {
@@ -20,7 +21,7 @@ class MyApp extends StatelessWidget {
     return const GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      home: GridViewComponent(),
+      home: IconComponent(),
     );
   }
 }
