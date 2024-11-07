fcffed16a810e34fd25f589c6c9eb6c2255080bc
moritejas <163624119+moritejas@users.noreply.github.com>
2024-11-07 14:24:09 +0530

    FlexComponent with Properties

--- lib/Component/Flex/flex.dart
+++ lib/Component/Flex/flex.dart
@@ -7,9 +7,51 @@ class FlexComponent extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        title: const Text("Flex Component Demo"),
       ),
-      body: const Text("This is Column Component"),
-
+      body: Flex(
+        direction: Axis.vertical, // This can be Axis.horizontal for row layout
+        mainAxisAlignment: MainAxisAlignment.center, // Aligns items along the main axis
+        crossAxisAlignment: CrossAxisAlignment.center, // Aligns items along the cross axis
+        mainAxisSize: MainAxisSize.max, // Controls the main axis size (max or min)
+        textDirection: TextDirection.ltr, // Controls text direction if using text widgets inside
+        verticalDirection: VerticalDirection.down, // Direction for vertical Flex (down or up)
+        children: <Widget>[
+          Container(
+            height: 50,
+            width: 200,
+            color: Colors.red,
+            child: const Center(
+              child: Text(
+                "Item 1",
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
+          Container(
+            height: 50,
+            width: 200,
+            color: Colors.blue,
+            child: const Center(
+              child: Text(
+                "Item 2",
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
+          Container(
+            height: 50,
+            width: 200,
+            color: Colors.green,
+            child: const Center(
+              child: Text(
+                "Item 3",
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 // import 'Homepage/homepage.dart';
-import 'Component/Expandable/expandable.dart';
+import 'Component/Flex/flex.dart';
 
 
 void main() {
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
     return const GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      home: ExpandableComponent(),
+      home: FlexComponent(),
     );
   }
 }
